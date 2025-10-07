db61ece0c0acec639dcc8e6c49aeb469ca906c15
khairudin86 <bocilsamba@gmail.com>
2025-10-07 14:35:36 +0800

    form dari hairan

---
+++ LatihanGithub/FormJurusan.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace LatihanGithub
+{
+    partial class FormJurusan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblidjurusan = new System.Windows.Forms.Label();
+            this.lbljurusan = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnsimpan = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnhapus = new System.Windows.Forms.Button();
+            this.btnedit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblidjurusan
+            // 
+            this.lblidjurusan.AutoSize = true;
+            this.lblidjurusan.Location = new System.Drawing.Point(278, 98);
+            this.lblidjurusan.Name = "lblidjurusan";
+            this.lblidjurusan.Size = new System.Drawing.Size(52, 13);
+            this.lblidjurusan.TabIndex = 0;
+            this.lblidjurusan.Text = "id jurusan";
+            this.lblidjurusan.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbljurusan
+            // 
+            this.lbljurusan.AutoSize = true;
+            this.lbljurusan.Location = new System.Drawing.Point(278, 134);
+            this.lbljurusan.Name = "lbljurusan";
+            this.lbljurusan.Size = new System.Drawing.Size(41, 13);
+            this.lbljurusan.TabIndex = 1;
+            this.lbljurusan.Text = "jurusan";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(375, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "JURUSAN";
+            // 
+            // btnsimpan
+            // 
+            this.btnsimpan.Location = new System.Drawing.Point(272, 171);
+            this.btnsimpan.Name = "btnsimpan";
+            this.btnsimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnsimpan.TabIndex = 3;
+            this.btnsimpan.Text = "simpan";
+            this.btnsimpan.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(361, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RPL",
+            "TKKR",
+            "DPB",
+            "TKI",
+            "HOTEL",
+            "AKL"});
+            this.comboBox1.Location = new System.Drawing.Point(361, 131);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // btnhapus
+            // 
+            this.btnhapus.Location = new System.Drawing.Point(361, 171);
+            this.btnhapus.Name = "btnhapus";
+            this.btnhapus.Size = new System.Drawing.Size(75, 23);
+            this.btnhapus.TabIndex = 6;
+            this.btnhapus.Text = "hapus";
+            this.btnhapus.UseVisualStyleBackColor = true;
+            // 
+            // btnedit
+            // 
+            this.btnedit.Location = new System.Drawing.Point(454, 171);
+            this.btnedit.Name = "btnedit";
+            this.btnedit.Size = new System.Drawing.Size(75, 23);
+            this.btnedit.TabIndex = 7;
+            this.btnedit.Text = "edit";
+            this.btnedit.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(219, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(360, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // FormJurusan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnedit);
+            this.Controls.Add(this.btnhapus);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnsimpan);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbljurusan);
+            this.Controls.Add(this.lblidjurusan);
+            this.Name = "FormJurusan";
+            this.Text = "FormJurusan";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblidjurusan;
+        private System.Windows.Forms.Label lbljurusan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnsimpan;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnhapus;
+        private System.Windows.Forms.Button btnedit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
